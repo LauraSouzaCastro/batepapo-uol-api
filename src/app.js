98ccb174be36473dadea92161655b8db5730e712
Laura <laurasouzacastro1@gmail.com>
2023-01-14 14:42:01 -0300

    POST /messages

--- src/app.js
+++ src/app.js
@@ -34,19 +34,45 @@ app.post("/participants", async (req, res) => {
 		if (participanteJaCadastrado) return res.status(409).send("Esse nome já está cadastrado!");
 	
 		await db.collection("participants").insertOne({ name: participante.name, lastStatus: Date.now() });
-		await db.collection("messages").insertOne({from: participante.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}` });
+		await db.collection("messages").insertOne({from: participante.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${dayjs().format('HH:mm:ss')}` });
 		res.sendStatus(201);
 	  } catch (err) {
 		console.log(err)
 		res.status(500).send("Deu algo errado no servidor")
 	  }
 });
-
 app.get("/participants", async (req, res) => {
 	db.collection("participants").find().toArray().then(dados => {
 		return res.send(dados)
 	}).catch(() => {
 		res.status(500).send("Deu erro no servidor de banco de dados")
 	});
+});
+
+const mensagemSchema = joi.object({
+	to: joi.string().required(),
+	text: joi.string().required(),
+	type: joi.string().valid('private_message', 'message').required()
+});
+app.post("/messages", async (req, res) => {
+	const mensagem = req.body;
+	const from = req.headers.user;
+	const validation = mensagemSchema.validate(mensagem);
+	if (validation.error) {
+		const errors = validation.error.details.map((detail) => detail.message);
+		return res.status(422).send(errors);
+	}
+	try {
+		const participanteCadastrado = await db.collection("participants").findOne({name: from});
+	
+		if (!participanteCadastrado) return res.status(422).send("User deve ser um participante existente na lista de participantes");
+	
+		await db.collection("messages").insertOne({from, to: mensagem.to, text: mensagem.text, type: mensagem.type, time: `${dayjs().format('HH:mm:ss')}` });
+
+		res.sendStatus(201);
+	  } catch (err) {
+		console.log(err)
+		res.status(500).send("Deu algo errado no servidor")
+	  }
 });
 app.listen(5000, () => console.log("Rodando..."));
