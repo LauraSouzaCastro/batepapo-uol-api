62f4549252a03a35b8cb5df393061922f50d13e7
Laura <laurasouzacastro1@gmail.com>
2023-01-14 13:52:09 -0300

    POST /participants

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import joi from 'joi';
+import dayjs from 'dayjs';
 dotenv.config();
 
 const app = express();
@@ -15,4 +17,28 @@ try {
 }
 const db = mongoClient.db();
 
+const participanteSchema = joi.object({
+	name: joi.string().required()
+});
+
+app.post("/participants", async (req, res) => {
+	const participante = req.body;
+	const validation = participanteSchema.validate(participante);
+	if (validation.error) {
+		const errors = validation.error.details.map((detail) => detail.message);
+		return res.status(422).send(errors);
+	}
+	try {
+		const participanteJaCadastrado = await db.collection("participants").findOne(participante);
+	
+		if (participanteJaCadastrado) return res.status(409).send("Esse nome já está cadastrado!");
+	
+		await db.collection("participants").insertOne({ name: participante.name, lastStatus: Date.now() });
+		await db.collection("messages").insertOne({from: participante.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}` });
+		res.sendStatus(201);
+	  } catch (err) {
+		console.log(err)
+		res.status(500).send("Deu algo errado no servidor")
+	  }
+}); 
 app.listen(5000, () => console.log("Rodando..."));
